0e896df26aaa66bf5fc5c76508e5afa08a30e33d
Harshikajain23 <jainharshika2002@gmail.com>
2025-11-21 16:55:28 +0530

    Final change

--- backend/src/routes/songRoute.js
+++ backend/src/routes/songRoute.js
@@ -1,20 +1,23 @@
+import express from 'express';
 import upload from "../controllers/middleware/multer.js";
-import { addSong, listSong , removeSong} from "../controllers/songContoller.js";
-import Song from "../models/songModel.js";
-
-import express from 'express'
+import { addSong, listSong, removeSong, increasePlayCount } from "../controllers/songContoller.js";
 
 const songRouter = express.Router();
 
-songRouter.post('/add',upload.fields([{name: 'image', maxCount: 1}, {name: 'audio', maxCount:1}]), addSong);
+// Add a song with image & audio
+songRouter.post(
+  '/add',
+  upload.fields([{ name: 'image', maxCount: 1 }, { name: 'audio', maxCount: 1 }]),
+  addSong
+);
+
+// List all songs
 songRouter.get('/list', listSong);
-songRouter.post('/remove', removeSong)
 
-import { increasePlayCount } from "../controllers/songContoller.js";
+// Remove a song
+songRouter.post('/remove', removeSong);
 
+// Increase play count
 songRouter.post('/increase-play/:id', increasePlayCount);
 
 export default songRouter;
-
-
-
