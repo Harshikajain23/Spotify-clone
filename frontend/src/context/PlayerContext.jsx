e8b25e7a4e240e46c0987993e9fa3f82c9a5d531
Harshikajain23 <129208900+Harshikajain23@users.noreply.github.com>
2025-11-21 17:14:47 +0530

    Update PlayerContext.jsx

--- frontend/src/context/PlayerContext.jsx
+++ frontend/src/context/PlayerContext.jsx
@@ -8,7 +8,7 @@ export const PlayerContextProvider = (props) => {
   const seekBg = useRef();
   const seekBar = useRef();
 
-  const url = "http://localhost:4000";
+  const url = "https://spotify-clone-r2qx.onrender.com";
 
   const [songsData, setSongsData] = useState([]);
   const [albumsData, setAlbumsData] = useState([]);
