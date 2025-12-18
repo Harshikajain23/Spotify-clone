628bf76aceca4820d64736c1bcf15c574649a571
Harshikajain23 <129208900+Harshikajain23@users.noreply.github.com>
2025-12-18 15:20:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,41 +14,31 @@ const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
-// // ===== CORS setup for frontend domains =====
-// const allowedOrigins = [
-//   "https://spotify-clone-frontend-hyn0.onrender.com",
-//   "https://spotify-clone-admin-528z.onrender.com",
-// ];
-
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // allow requests with no origin (like Postman)
-//       if (!origin) return callback(null, true);
-
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         const msg =
-//           "The CORS policy for this site does not allow access from the specified Origin.";
-//         return callback(new Error(msg), false);
-//       }
-
-//       return callback(null, true);
-//     },
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
+// ===== CORS setup for frontend domains =====
+const allowedOrigins = [
+  "https://spotify-clone-frontend-hyn0.onrender.com",
+  "https://spotify-clone-admin-528z.onrender.com",
+];
 
 app.use(
   cors({
-    origin: [
-      "https://spotify-clone-frontend-hyn0.onrender.com",
-      "https://spotify-clone-admin-528z.onrender.com"
-    ],
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // only needed if using cookies/auth
   })
 );
+
 
 // ===== Middlewares =====
 app.use(express.json());
