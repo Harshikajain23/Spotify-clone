50ea49c6a0aac0019b4d0c2718adba70c09b01ac
Harshikajain23 <129208900+Harshikajain23@users.noreply.github.com>
2025-12-18 18:43:49 +0530

    Update Navbar.jsx

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='navbar w-full border-b-2 border-gray-800 px-5 sm:px-12 py-4 text-lg'>
+    <div className='navbar flex justify-between w-full border-b-2 border-gray-800 px-5 sm:px-12 py-4 text-lg'>
         <p>Admin panel</p>
+       <a href="https://spotify-clone-frontend-hyn0.onrender.com" className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">
+         Back to Home </a>
     </div>
   )
 }
