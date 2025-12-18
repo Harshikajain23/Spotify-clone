55159d62d21bc2008f636f5c4f2157b1ab48e85c
Harshikajain23 <129208900+Harshikajain23@users.noreply.github.com>
2025-12-18 18:49:20 +0530

    Update Navbar.jsx

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,7 +4,9 @@ const Navbar = () => {
   return (
     <div className='navbar flex justify-between w-full border-b-2 border-gray-800 px-5 sm:px-12 py-4 text-lg'>
         <p>Admin panel</p>
-       <a href="https://spotify-clone-frontend-hyn0.onrender.com" className="text-[15px] px-4 py-1 hidden md:block cursor-pointer">
+       <a href="https://spotify-clone-frontend-hyn0.onrender.com" className="text-[15px] px-4 py-1 hidden md:block cursor-pointer"
+            target="_blank"   // optional: opens in a new tab
+  rel="noopener noreferrer">
          Back to Home </a>
     </div>
   )
