b15d2de7b61a16ee2ee24829faae8688214710d0
Harshikajain23 <129208900+Harshikajain23@users.noreply.github.com>
2026-02-06 22:32:18 +0530

    Update songModel.js

--- backend/src/controllers/models/songModel.js
+++ backend/src/controllers/models/songModel.js
@@ -1,20 +1,19 @@
 import mongoose from "mongoose";
 
-const songSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    desc: {type: String, required:true},
-    album: {type: String, required: true},
-    image: {type: String, required: true},
-    playCount: {type: Number, default:0},
-    file: {type: String, required: true},
-    duration: {type: String, required: true},
-
- {
-    timestamps: true 
+const songSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    desc: { type: String, required: true },
+    album: { type: String, required: true },
+    image: { type: String, required: true },
+    playCount: { type: Number, default: 0 },
+    file: { type: String, required: true },
+    duration: { type: String, required: true },
+  },
+  {
+    timestamps: true, 
   }
-
-
-})
+);
 
 const songModel = mongoose.models.song || mongoose.model("song", songSchema)
 
